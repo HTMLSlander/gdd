4c14381abf638d860d2e5427c57c1f029fef699d
Hcode7 <ratitamine3@gmail.com>
2025-03-22 01:39:19 +0000

    Added postgres db

--- hydration_reminder/settings.py
+++ hydration_reminder/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost' , '127.0.0.1', 'https://getdailydrink.up.railway.app']
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = [ 'https://getdailydrink.up.railway.app/' ] 
+CSRF_TRUSTED_ORIGINS = [ 'https://getdailydrink.up.railway.app' ] 
 
 # Application definition
 
