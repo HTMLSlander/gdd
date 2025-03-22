56c624e7a175fcc7f1ec77dd7df3b19c29eeacc3
Hcode7 <ratitamine3@gmail.com>
2025-03-22 00:45:36 +0000

    Added postgres db

--- hydration_reminder/settings.py
+++ hydration_reminder/settings.py
@@ -118,8 +118,7 @@ WSGI_APPLICATION = 'hydration_reminder.wsgi.application'
 from decouple import config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
+        'ENGINE': 'django.db.backends.postgresql',        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
